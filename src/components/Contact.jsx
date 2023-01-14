e3a07f8ab0338ba80ff523a48c80bbe9c1c66ba9
imanishi.naoki <imanishi53web@gmail.com>
2023-01-15 05:41:37 +0900

    デザイン修正

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <section>
       <div id="contact" className="contact">
         <div className="container mx-auto py-16">
-          <h2 className="section-title">
+          <h2 className="section-title -white">
             CONTACT<span>お問い合せ</span>
           </h2>
           <div className="list-all-btn text-right">
